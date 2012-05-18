3551df85109f950ce890623f1dd6497ef43f2251
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-05-18 12:39:05 -0700

    Add 2012 to copyrights.

--- molt/test/__init__.py
+++ molt/test/__init__.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-# Copyright (C) 2011 Chris Jerdonek. All rights reserved.
+# Copyright (C) 2011-2012 Chris Jerdonek. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
