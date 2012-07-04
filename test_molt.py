39aa3b3403c308017cb1e1c81fc877680b8d530c
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-07-04 06:22:20 -0700

    Add code comment.

--- test_molt.py
+++ test_molt.py
@@ -24,11 +24,16 @@ def main(argv=None):
     source_dir = os.path.dirname(__file__)
 
     # Modify argv in place.
+    #
     # Insert the test option after the program name but before all other
     # arguments.  In this way, test filters should always appear at the
     # beginning of the argument list in this form of invoking the
     # script in test mode.
     argv.insert(1, OPTION_MODE_TESTS[0])
+
+    # Since this script exists in the source folder but not in the
+    # package folder, it can only be run in the presence of source.
+    # Pass the source information along to indicate this..
     argv += [OPTION_SOURCE_DIR[0], source_dir]
 
     return molt.main(sys_argv=argv, from_source=True)
